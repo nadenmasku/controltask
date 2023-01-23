a804fe7c72d3c2c35bcd20a2653411f36fcbd25c
redir <redirectyoung@gmail.com>
2023-01-23 21:34:34 +0300

    Start

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//Опишем метод сортировки
+void SortMethod(string[]){
+    
+}
+
+
+
+
+
+
+
+
+
+// Зададим первоначальный массив
+string[] toSortArray1 = { "hello", "2", "world", ":-)" };
+// Выведем массив в консоль
+Console.WriteLine(toSortArray1);
+Console.WriteLine();
