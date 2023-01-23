aa544a6fd4679d01a42f53124f3395146a45841c
redir <redirectyoung@gmail.com>
2023-01-23 21:48:11 +0300

    test version

--- task/Program.cs
+++ task/Program.cs
@@ -1,6 +1,18 @@
 ﻿//Опишем метод сортировки
-void SortMethod(string[]){
-    
+string[] SortMethod(string[] array)
+{
+    int size = 0;
+    string[] sortedarray = new string [size];
+    for (int i = 0; i < array.Length;i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Array.Resize(ref sortedarray, sortedarray.Length + 1);
+            sortedarray[i] = array[i];
+        }
+    }
+
+    return sortedarray;
 }
 
 
@@ -16,3 +28,5 @@ string[] toSortArray1 = { "hello", "2", "world", ":-)" };
 // Выведем массив в консоль
 Console.WriteLine(toSortArray1);
 Console.WriteLine();
+//Выведем отсортированный массив
+Console.WriteLine(SortMethod(toSortArray1));
